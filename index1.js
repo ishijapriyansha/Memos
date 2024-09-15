7de7689a5a5fde8c3ad7ef2a7c7bcb9f0daa87ba
Ishija Priyansha <ishijapriyansha19@gmail.com>
2024-09-15 23:12:22 +0530

    chore:create schema and routes

--- index1.js
+++ index1.js
@@ -5,9 +5,13 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.get('/ishija', (req, res) => {
+    res.send('Hello Ishija!')
+  })
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/memos',require('./routes/memos'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
