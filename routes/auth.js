8df2a066ed382bfa23253ef976ffebec30e09b78
Ishija Priyansha <ishijapriyansha19@gmail.com>
2024-09-16 02:48:54 +0530

    chore:add express validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,21 @@
 const express =require("express")
 const User =require('../models/User')
 const router=express.Router();
+const { validationResult,body } = require('express-validator');
 
-router.post('/',(req,res)=>{
-   console.log(req.body);
-   const user =User(req.body);
-   user.save();
-  res.send(req.body);
+router.post('/',[body('name','Enter a valid name').isLength({min:3}),
+                body('email','Enter a valid email').isEmail(),
+                body('password','Password must be atleast 3 characters').isLength({min:3})
+],async(req,res)=>{
+   const errors=validationResult(req);
+   if(!errors.isEmpty()){
+    return res.status(400).json({errors:errors.array()});
+   }
+   const user= await User.create({
+      name:req.body.name,
+      email:req.body.email,
+      password:req.body.password
+}); 
+res.json(user);
 })
 module.exports=router
