62553d2a5b0ced41de895322c5f64cdbbadd4f91
Ishija Priyansha <ishijapriyansha19@gmail.com>
2024-09-15 20:36:12 +0530

    connect to mongo

---
+++ db.js
@@ -0,0 +1,8 @@
+const mongoose= require("mongoose");
+const mongoURI="mongodb://127.0.0.1:27017/"
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI).then(()=>console.log("Connected")).catch((e)=>console.log(e.message))
+}
+
+module.exports=connectToMongo
