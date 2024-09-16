c4281197ef048341acf99d5297fb584197cfcebf
Ishija Priyansha <ishijapriyansha19@gmail.com>
2024-09-16 12:12:12 +0530

    add try catch block

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const User =require('../models/User')
 const router=express.Router();
 const { validationResult,body } = require('express-validator');
 
-router.post('/',[body('name','Enter a valid name').isLength({min:3}),
+router.post('/createuser',[body('name','Enter a valid name').isLength({min:3}),
                 body('email','Enter a valid email').isEmail(),
                 body('password','Password must be atleast 3 characters').isLength({min:3})
 ],async(req,res)=>{
@@ -11,11 +11,19 @@ router.post('/',[body('name','Enter a valid name').isLength({min:3}),
    if(!errors.isEmpty()){
     return res.status(400).json({errors:errors.array()});
    }
+try{
+   
    const user= await User.create({
       name:req.body.name,
       email:req.body.email,
       password:req.body.password
 }); 
 res.json(user);
-})
+}
+catch(error){
+   console.error(error.message)
+   res.status(500).send("Some error occurred");}
+
+}
+)
 module.exports=router
