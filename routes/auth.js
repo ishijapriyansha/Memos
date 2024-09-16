3c1c341bc0c015f21c4bff0ec24cf91627ec22d2
Ishija Priyansha <ishijapriyansha19@gmail.com>
2024-09-16 13:56:40 +0530

    add bcrypt and jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,10 @@ const express =require("express")
 const User =require('../models/User')
 const router=express.Router();
 const { validationResult,body } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET="ishija$1"
+     
 router.post('/createuser',[body('name','Enter a valid name').isLength({min:3}),
                 body('email','Enter a valid email').isEmail(),
                 body('password','Password must be atleast 3 characters').isLength({min:3})
@@ -12,13 +15,22 @@ router.post('/createuser',[body('name','Enter a valid name').isLength({min:3}),
     return res.status(400).json({errors:errors.array()});
    }
 try{
+
+   const salt=await bcrypt.genSalt(10);
+   const secPass=await bcrypt.hash(req.body.password,salt);
    
    const user= await User.create({
       name:req.body.name,
       email:req.body.email,
-      password:req.body.password
+      password:secPass
 }); 
-res.json(user);
+const data={
+   user:{
+      id:user.id
+   }
+}
+var token = jwt.sign(data, JWT_SECRET);   
+res.json(token);
 }
 catch(error){
    console.error(error.message)
