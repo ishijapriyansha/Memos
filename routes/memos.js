cb87258b7be863cdc879a350673ec1ba0db2961a
Lenovo <ishijapriyansha19@gmail.com>
2025-02-07 01:37:01 +0530

    add delete endpoint

--- routes/memos.js
+++ routes/memos.js
@@ -2,7 +2,8 @@ const express = require("express")
 const router = express.Router();
 const fetchUser = require("../middleware/fetchUser")
 const Memos = require("../models/Memos")
-const { body, validationResult } = require('express-validator')
+const { body, validationResult } = require('express-validator');
+const { findByIdAndDelete } = require("../models/User");
 //Route 1: /api/memos/fetchallmemos:uses GET request to fetch all memos
 router.get('/fetchallmemos', fetchUser, async (req, res) => {
     try {
@@ -41,9 +42,10 @@ router.post('/addmemo', fetchUser, [
 
     //update a memo. login needed.
 
-    router.post('/updatememo/:id', fetchUser, async (req , res)=>{
+    router.put('/updatememo/:id', fetchUser, async (req , res)=>{
+        try {
         const {title, description, tag} = req.body
-        const newMemo=[];
+        const newMemo={};
         if(title){ newMemo.title=title}
         if(description){ newMemo.description=description}
         if(tag){newMemo.tag=tag}
@@ -56,10 +58,29 @@ router.post('/addmemo', fetchUser, [
         //to ensure somebody is not trying to edit memos on some other user's account
         if(memo.user.toString()!=req.user.id){return res.status(401).send("Not allowed!") }
 
-        memo= await NotBeforeError.findByIdAndUpdate(req,params.id, {$set: newMemo}, {new:true})
+        memo= await Memos.findByIdAndUpdate(req.params.id, {$set: newMemo}, {new:true})
         res.json({memo});
-    }
+        }
+        catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+    })
 
-    )
+    router.delete('/deletememo/:id',fetchUser, async (req,res)=>{
+        try{
+
+        
+        let memo= await Memos.findById(req.params.id);
+        if(!memo) {return res.status(404).send("Not found")}
+        if(memo.user.toString()!=req.user.id){return res.status(401).send("Not allowed!") }
+        memo= await Memos.findByIdAndDelete(req.params.id)
+        res.json({"Success":"Memo deleted", memo:memo});  //memo:memo= "memo":memo or {memo} 
+        }
+        catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+    })
 
 module.exports = router
