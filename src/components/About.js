ae184b26bc816e976537108e216a7d25dde72868
Lenovo <ishijapriyansha19@gmail.com>
2025-02-07 16:23:17 +0530

    remove warnings

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ export default function About() {
     const a =useContext(MemoContext)
     useEffect(()=>{
         a.update();
+        // eslint-disable-next-line
     },[])
   return (
     <div>About {a.state.name} and her class {a.state.class}</div>
